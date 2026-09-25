64d1f0558e33aadbecf1c2a164c5472352223367
chanka-herath2001 <cb010779@students.apiit.lk>
2026-09-25 13:30:49 +0530

    config added for vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/serendib-multisport/',
   plugins: [react(), tailwindcss()],
 })
